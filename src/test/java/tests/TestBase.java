5740df5f21c839d0722fa705078cab09147b9ac2
Roman Bragin <bragin-96@bk.ru>
2025-03-12 12:50:22 +0500

    Добавлен TestBase с конфигурациями для запуска

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
     @BeforeAll
     static void configuration() {
         Configuration.browserSize = "1920x1080";
